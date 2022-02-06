f4b591518f094c83e89e671fa09e81b9c139e38f
mbe81 <mbe81@users.noreply.github.com>
2022-02-06 09:52:19 +0000

    deploy: f1bf6341bc3602893e5c309af4116ba202a66dd8

---
+++ docs/precache-manifest.a94bbd9212d3fc60263f9cb171559278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94072f307d50aacbd9a8e7564b814c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32876136f6e0604bc3b7",
+    "url": "/static/css/main.0900f9ac.chunk.css"
+  },
+  {
+    "revision": "63105514928b1e9c327b",
+    "url": "/static/js/2.42a1d8b5.chunk.js"
+  },
+  {
+    "revision": "32876136f6e0604bc3b7",
+    "url": "/static/js/main.77917b89.chunk.js"
+  },
+  {
+    "revision": "fdd917193a51f856fb51",
+    "url": "/static/js/runtime-main.f5288c10.js"
+  },
+  {
+    "revision": "a247d02f190004e3e37b80511af40cca",
+    "url": "/static/media/logo.a247d02f.png"
+  }
+]);
